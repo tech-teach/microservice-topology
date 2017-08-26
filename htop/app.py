22cb211e71a7cbc88aba877295445996fb0955b1
Oscar David Arbeláez Echeverri <odarbelaeze@gmail.com>
2017-08-26 16:39:36 -0500

    Fix htop error handling

--- htop/app.py
+++ htop/app.py
@@ -1,6 +1,6 @@
 from apistar.frameworks.wsgi import WSGIApp as App
 from apistar.http import Response, Request, QueryParam
-from apistar import Route
+from apistar import Route, Component
 from psutil import cpu_percent
 
 
@@ -22,12 +22,27 @@ class CORSApp(App):
         return Response(content, status, headers, content_type)
 
 
-def cpu(interval: QueryParam):
+class Interval(object):
+
+    def __init__(self, value):
+        self.value = float(value)
+        if self.value < 0:
+            raise ValueError('Interval must be positive.')
+
+
+def fetch_interval(interval: QueryParam):
+    try:
+        return Interval(interval)
+    except ValueError:
+        return None
+
+
+def cpu(interval: Interval):
     """
     Retrieves the cpu percentage in an interval of time.
     """
     # TODO Return an error in case the query param cannot be casted to float
-    interval = float(interval or 1.0)
+    interval = interval.value if interval else 1.0
     return {
         'cpu': cpu_percent(interval=interval, percpu=True),
         'interval': interval
@@ -38,5 +53,9 @@ ROUTES = [
     Route('/htop', 'GET', cpu),
 ]
 
+COMPONENTS = [
+    Component(Interval, init=fetch_interval)
+]
 
-app = CORSApp(routes=ROUTES)
+
+app = CORSApp(routes=ROUTES, components=COMPONENTS)
