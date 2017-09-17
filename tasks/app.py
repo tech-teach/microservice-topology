668fc479b8faf06964ccdcb152d92bb9f39cbff6
Oscar David Arbeláez Echeverri <odarbelaeze@gmail.com>
2017-09-17 14:01:21 -0500

    Add placeholders for tasks

--- tasks/app.py
+++ tasks/app.py
@@ -9,31 +9,54 @@ from sanic import Sanic
 from sanic_cors import CORS
 import metrics as mt
 
+
 app = Sanic(__name__)
 CORS(app)
 
 
 @app.route('/tasks', methods=['POST', 'OPTIONS'])
 def tasks(request):
+    """
+    Receive a file and enqueue processing.
+    """
+
     if request.method == 'OPTIONS':
         return json({})
-    try:
-        request_file = request.files.get('file').body.decode('unicode_escape')
-    except Exception as e:
-        return json(
-            {'error': 'File has not been found', 'message': f'{e}'},
-            status=400
-        )
 
-    file_handler = io.StringIO(request_file)
+    file = request.files.get('file')
+    if file is None:
+        return json({'error': 'A file is required'}, status=400)
 
-    response = mt.get_accuracy(file_handler, 4)
+    # Further validate file if need be
 
+    # Write file to the filesystem
+
+    # Create Task row in the database
+
+    # Enqueue task excecution with the uuid
+
+    # Return to the user with the task uid and 201 created
+
+    return json({'uid': uuid.uuid4().hex}, status=201)
+
+
+@app.route('/tasks/<uid>', methods=['GET', 'OPTIONS'])
+def task(request, uid):
+    """
+    Inform the user about a tasks status.
+    """
+    # Find task by uid
+    # If not found, you'now not found
+    # return task status
     return json({
-        "uuid": uuid.uuid4().hex,
-        # "response": response
+        'uid': uid,
+        'status': 'complete',
+        'errors': None,
+        'accuracies': {
+            'something': 1.0,
+            'something_else': 0.5,
+        },
     })
-
 
 
 if __name__ == "__main__":
